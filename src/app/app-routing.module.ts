e755ba77dbee2fc13d6021dcdc725f059148693f
Caio Vinicius Rodrigues <caio359@ica.com>
2020-09-25 08:08:18 -0300

    restaurant-detail corrigido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,19 @@ import { RestaurantDetailComponent } from 'app/restaurants/restaurant-detail/res
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { MenuComponent } from './restaurants/restaurant-detail/menu/menu.component';
+import { ReviewsComponent } from './restaurants/restaurant-detail/reviews/reviews.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'restaurants', component: RestaurantsComponent },
-  { path: 'restaurants/:id', component: RestaurantDetailComponent },
+  { path: 'restaurants/:id', component: RestaurantDetailComponent,
+    children: [
+      { path: '', redirectTo: 'menu', pathMatch: 'full' },
+      { path: 'menu', component: MenuComponent },
+      { path: 'reviews', component: ReviewsComponent },
+  ]},
 ];
 
 @NgModule({
